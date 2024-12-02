f92ce43927fae68c3da6b4bc79c0f296ad636312
Andrew Portner <aportner@gmail.com>
2024-12-01 19:10:33 -0800

    now rendering multiple points

--- NAPLPS/Commands/PointCommand.cs
+++ NAPLPS/Commands/PointCommand.cs
@@ -1,24 +1,27 @@
 // Copyright (c) 2024 FoxCouncil - https://github.com/FoxCouncil/NAPLPS
 
+using System.Diagnostics;
+
 namespace NAPLPS.Commands;
 
 public abstract class PointCommand : FillableGeometricDrawingCommandBase
 {
-    public Vector3 Point { get; internal set; }
-
-
     public bool IsVisible { get; internal set; }
 
     public PointCommand(NaplpsState state, NaplpsCommands opcode, NaplpsOperands operands) : base(state, opcode, operands)
     {
-        Point = ProcessVertices(operands).FirstOrDefault();
+        var vertices = ProcessVertices(operands);
 
-        if (opcode == POINT_ABS || opcode == POINT_SET_ABS)
+        foreach (var point in vertices)
         {
-            SetPen(Point);
-        } else
-        {
-            SetPen(state.Pen + Point);
+            if (opcode == POINT_ABS || opcode == POINT_SET_ABS)
+            {
+                SetPen(point);
+            }
+            else
+            {
+                SetPen(state.Pen + point);
+            }
         }
 
         IsVisible = opcode == POINT_ABS || opcode == POINT_REL;
